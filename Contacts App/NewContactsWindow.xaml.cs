b96ab3659aecabfebdd12f4da3aa96264756b7c6
Panlo F Machado <pablo.fmachado92@gmail.com>
2019-09-09 12:27:36 -0300

    Contacts App
    Database read and write.
    Initial Layouting work done.

---
+++ Contacts App/NewContactsWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Contacts_App.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Contacts_App
+{
+    /// <summary>
+    /// Interaction logic for NewContactsWindow.xaml
+    /// </summary>
+    public partial class NewContactsWindow : Window
+    {
+        public NewContactsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text
+
+            };
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+
+           Close();
+
+        }
+    }
+}
